8975fc78d2dcd3f7d3db011e9559f8d406db8f68
Marius Colacioiu <marius.colacioiu@gmail.com>
2023-04-10 23:21:26 +0200

    Done bookmark website restyle

--- website-projects/bookmark/js/script.js
+++ website-projects/bookmark/js/script.js
@@ -1,13 +1,13 @@
 const tabs = document.querySelectorAll('.tab');
 const panels = document.querySelectorAll('.panel');
-
+const btn = document.getElementById('menu-btn');
+const logo = document.getElementById('logo');
 
 // Tabs menu event listener
 
 tabs.forEach(tab => {
     tab.addEventListener('click', onTabClick);
 })
-
 function onTabClick(e) {
     // Deactivate all tabs
     tabs.forEach(tab => {
@@ -20,4 +20,19 @@ function onTabClick(e) {
     const selectedPanel = e.target.getAttribute('data-target');
     const panelElement = document.getElementById('panels').getElementsByClassName(selectedPanel)[0];
     panelElement.classList.remove('hidden');
+}
+
+// Hamburger button event listener
+btn.addEventListener('click', navToggle);
+
+function navToggle() {
+    btn.classList.toggle('open');
+    menu.classList.toggle('flex');
+    menu.classList.toggle('hidden');
+    const isMenuOpen = menu.classList.contains('flex');
+    if (isMenuOpen) {
+        logo.setAttribute('src', './images/logo-bookmark-footer.svg');
+    } else {
+        logo.setAttribute('src', './images/logo-bookmark.svg');
+    }
 }
