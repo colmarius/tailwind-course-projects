9e44acd4b303bb864fad94d4b522bd6ff8dff6cb
Marius Colacioiu <marius.colacioiu@gmail.com>
2023-04-20 08:45:28 +0200

    Partially done styling shortly

--- website-projects/shortly/js/script.js
+++ website-projects/shortly/js/script.js
@@ -1,11 +1,11 @@
-const btn = document.getElementById('menu-btn')
-const menu = document.getElementById('menu')
+// const btn = document.getElementById('menu-btn')
+// const menu = document.getElementById('menu')
 
 const input = document.getElementById('link-input')
 const linkForm = document.getElementById('link-form')
 const errMsg = document.getElementById('err-msg')
 
-btn.addEventListener('click', navToggle)
+// btn.addEventListener('click', navToggle)
 linkForm.addEventListener('submit', formSubmit)
 
 // Toggle Mobile Menu
